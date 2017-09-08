682c48b8032736517814597f653b4a205679a9ca
niki-s <nsilveria@nevada.unr.edu>
2017-09-08 14:25:25 -0700

    preliminary homework 1 files, hc_eval works for part 1a

---
+++ hc_eval.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdlib.h>
+
+//using namespace std;
+
+double eval(int* pj);
+int* modifyVec(int* previousVec);
+
+int main(){
+	int* bestVec = new int[100];
+	//int bestVec[100];
+	int* vec;
+	double bestFitness = 0;
+	int fill;
+	int i = 0;
+
+	double fitness;
+
+	for(fill = 0; fill < 100; fill++){
+		bestVec[fill] = 0;
+	}
+
+	bestFitness = eval(bestVec);
+
+	while(bestFitness < 100){
+		vec = modifyVec(bestVec);
+		fitness = eval(vec);
+
+		if (fitness > bestFitness){
+			bestVec = vec;
+			bestFitness = fitness;
+		}
+		i++;
+	}
+
+	std::cout << "best fitness = " << bestFitness << std::endl;
+	std::cout << "in total iterations: " << i << std::endl;
+	std::cout << "with vector: " << std::endl;
+	for(i = 0; i < 100; i++){
+		std::cout << bestVec[i];
+	}
+	std::cout << std::endl;
+
+	//delete vec;
+	delete bestVec;
+	}
+
+int* modifyVec(int* previousVec){
+ 	int* newVec = new int[100];
+ 	//fill the new victor with the same values as the previous one
+ 	for (int i = 0; i < 100; ++i){
+ 		newVec[i] = previousVec[i];
+ 	}
+	//generate a random index from between 0 and 99
+	int randIndex = rand() % 100;
+
+	//reference this index of the vector and flip the bit, return new value
+	newVec[randIndex] = 1 - newVec[randIndex];
+	return newVec;
+}
